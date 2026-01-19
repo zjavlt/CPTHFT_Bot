2a4ef1907f5362128a37a4d09bac2255c8638fcc
COMPETE <yunkim1129@gmail.com>
2026-01-19 17:51:15 -0500

    Using abstraction for split companies

---
+++ BinanceConnector.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "MarketDataConnector.hpp"
+#include <simdjson.h>
+
+class BinanceConnector : public MarketDataConnector {
+private:
+    simdjson::ondemand::parser parser_;
+    simdjson::padded_string json_data_;
+
+public:
+    using MarketDataConnector::MarketDataConnector;
+
+protected:
+    void on_session_started() override {
+        // no need
+    }
+
+    void process_message(std::string_view data) override {
+        json_data_ = simdjson::padded_string(data);
+        try {
+            auto doc = parser_.iterate(json_data_);
+            std::string_view event_type = doc["e"].get_string();
+
+            if (event_type == "trade") {
+                Trade t;
+                t.symbol = doc["s"].get_string();
+                
+                // could use simdjson double casting (fast)
+                std::string_view p_str = doc["p"].get_string();
+                t.price = std::stod(std::string(p_str));
+
+                std::string_view q_str = doc["q"].get_string();
+                t.quantity = std::stod(std::string(q_str));
+
+                t.event_time = doc["E"].get_int64();
+                t.trade_time = doc["T"].get_int64();
+
+
+                if (!queue_->enqueue(t)) {
+                    std::cerr << "Queue Full" << std::endl;
+                }
+            }
+        }catch (simdjson::simdjson_error&) {}
+    }
+};
