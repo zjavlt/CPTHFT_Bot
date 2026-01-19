c2df65d4fdae229f2fd25e83af8234f71effec69
COMPETE <yunkim1129@gmail.com>
2026-01-18 20:53:50 -0500

    Finished constructor, should do enqueue/dequeue

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -25,5 +25,23 @@ private:
     };
 
     //member var
-    const size_t
+    const size_t buffer_mask_; // size - 1 for bit computation
+    Element* buffer_;
+
+    //alignas: allocate these vars in independant cache lines
+    alignas(CACHE_LINE_SIZE) std::atomic<size_t> tail_; // producer location
+    alignas(CACHE_LINE_SIZE) std::atomic<size_t> head_; // consumer location
+
+public:
+    //size must be 2**n
+    RingBuffer(size_t size) 
+        : buffer_mask_(size - 1), buffer_(new Element[size]), tail_(0), head_(0)
+    {
+        // slot reset
+        for (size_t i = 0; i < size; ++i) {
+            buffer_[i].sequence.store(i, std::memory_order_relaxed);
+        }
+    }
+
+    ~RingBuffer() { delete[] buffer_;}
 }
