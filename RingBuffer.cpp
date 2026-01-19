e097f737b86a54da55693bccb9d842d3f6a82d5f
COMPETE <yunkim1129@gmail.com>
2026-01-19 01:55:41 -0500

    Ring Buffer final

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -29,6 +29,7 @@ private:
     Element* buffer_;
 
     //alignas: allocate these vars in independant cache lines
+    //also to prevent false sharing
     alignas(CACHE_LINE_SIZE) std::atomic<size_t> tail_; // producer location
     alignas(CACHE_LINE_SIZE) std::atomic<size_t> head_; // consumer location
 
@@ -44,4 +45,75 @@ public:
     }
 
     ~RingBuffer() { delete[] buffer_;}
-}
+
+    //enqueue
+    bool enqueue(T data) {
+        size_t cell_sequence;
+        size_t pos = tail_.load(std::memory_order_relaxed); //왜 memory_order_relaxed야 제일 약한 memory ordering이라는데
+
+        while (true) {
+            // slot check
+            Element& cell = buffer_[pos & buffer_mask_]; // faster modulo with bit
+
+            cell_sequence = cell.sequence.load(std::memory_order_acquire);
+
+            intptr_t dif = (intptr_t)cell_sequence - (intptr_t)pos;
+
+            if (dif == 0) {
+
+                if (tail_.compare_exchange_weak(pos, pos + 1, std::memory_order_relaxed)) {
+                    //why weak exchange?
+
+                    //slot successfully taken
+                    cell.data = data;
+
+                    //update sequence number
+                    // using release ensures data is updated first. 
+                    // without it, CPU may swap these lines and other threads may read dummy data
+                    cell.sequence.store(pos + 1, std::memory_order_release);
+                    return true;
+                }
+            }
+            //room sequence is smaller than my sequence -> consumer did not read and update (occupied)
+            else if (dif < 0) {
+                return false;
+            }
+            // dif > 0: other producer already passed by -> load tail again
+            else {
+                pos = tail_.load(std::memory_order_relaxed);
+            }
+        }
+    }
+
+    bool dequeue(T& data) {
+        size_t cell_sequence;
+        size_t pos = head_.load(std::memory_order_relaxed);
+
+        while (true) {
+            Element& cell = buffer_[pos & buffer_mask_];
+            cell_sequence = cell.sequence.load(std::memory_order_acquire); // acquire receives release
+            
+            //check if sequence is pos + 1 after producer used data
+            intptr_t dif = (intptr_t)cell_sequence - (intptr_t)(pos + 1);
+
+            if (dif == 0) {
+                //data is ready
+                // head +1 so that I can process it
+                if (head_.compare_exchange_weak(pos, pos + 1, std::memory_order_relaxed)) {
+                    data = cell.data;
+
+                    cell.sequence.store(pos + buffer_mask_ + 1, std::memory_order_release);
+                    return true;
+                }
+            }
+            // data not ready (producer did not use yet)
+            else if (dif < 0) {
+                return false; //queue empty
+            }
+            // head already changed (ABA). update position to the new head
+            else {
+                pos = head_.load(std::memory_order_relaxed);
+            }
+        }
+    }
+};
