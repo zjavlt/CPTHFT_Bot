5bdc6ea043439b620f4ca14c9ea08c4f018e6760
COMPETE <yunkim1129@gmail.com>
2026-01-20 17:04:48 -0500

    Some print fixes

--- main.cpp
+++ main.cpp
@@ -22,17 +22,20 @@ int main() {
         Trade t;
         while (true) {
             if (queue->dequeue(t)) {
-                std::string ex_name = (t.exchange == ExchangeId::BINANCE) ? "BIN" : "CB";
+                std::string ex_name = (t.exchange == ExchangeId::BINANCE) ? "BIN" : "C B";
 
-                int64_t latency = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::system_clock::now().time_since_epoch()
-                ).count() - t.trade_time;
+                // int64_t latency = std::chrono::duration_cast<std::chrono::milliseconds>(
+                //     std::chrono::system_clock::now().time_since_epoch()
+                // ).count() - t.trade_time;
+                int64_t serverLat = t.event_time - t.trade_time;
                 std::cout << std::fixed << std::setprecision(8)
                           << "[" << ex_name << "] "
                           << t.symbol
                           << " | P: " << t.price
                           << " | Q: " << t.quantity
-                          << " | Lat: " << latency << "ms"
+                        //   << " | Lat: " << latency << "ms"
+                          << " | Server Lat: " << serverLat << "ms"
+                          << " | Time: " << t.event_time
                           // WSL time delay not resolvable
                           // TODO: integrate internal delay calculation for latency (if heavy compute -> drop)
                           << std::endl;
